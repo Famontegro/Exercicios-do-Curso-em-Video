872b47bd914b96e8962f29a632886f81dca4233c
Rafamonte <139199761+Famontegro@users.noreply.github.com>
2026-09-13 01:25:58 -0300

    Atualizar o CALCULADORA DE SOMA TOP TOP.py

--- CALCULADORA DE SOMA TOP TOP.py
+++ CALCULADORA DE SOMA TOP TOP.py
@@ -2,4 +2,3 @@ n1 = int(input('Digite um numero: '))
 n2 = int(input('Digite mais um numero: '))
 s = n1 + n2
 print('A soma entre {} e {} vale {}'.format(n1, n2, s))
-#Vai tomar no cu Gabriel, ve o bagulho direto ao invês de ver só o código
