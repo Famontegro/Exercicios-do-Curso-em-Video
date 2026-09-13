29baa0dd0058612dd71f604a8c0b23c4dc767ffe
Rafamonte <139199761+Famontegro@users.noreply.github.com>
2026-09-13 01:26:25 -0300

    Atualizar o Caracteristicas do que você digitou.py

--- Caracteristicas do que você digitou.py
+++ Caracteristicas do que você digitou.py
@@ -3,4 +3,3 @@ print('O que você digitou é numérico:', algo.isnumeric())
 print('O que você digitou é alfabético:', algo.isalpha())
 print('O que você digitou é numérico ou alfabético:', algo.isalnum())
 print('O que você digitou é somente em letra maiuscula:', algo.isupper())
-#Vai tomar no cu Gabriel, ve o bagulho direto ao invês de ver só o código
